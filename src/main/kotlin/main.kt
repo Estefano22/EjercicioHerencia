2bfc497c76a1ee8579072e3d479464589c31b7f0
Estefano22 <sandulescustefano8@gmail.com>
2021-04-23 13:39:31 +0200

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,30 @@
+fun main(){
+    val Especialidad1 = Especialidad("esgrima")
+    val Especialidad2 = Especialidad("maraton")
+    val Especilaidad3 = Especialidad("natacion")
+
+    //val listaMacotas = mutableListOf(Mascota1, Mascota2)
+
+
+}
+
+class Participantes(var nombre : Nombre,) {
+
+
+    fun escribirEspecialidad() {
+
+
+
+    }
+
+}
+
+
+class Nombre(var nombre: String){
+
+}
+
+class Especialidad (var nombreEspecialidad: String){
+
+}
+
